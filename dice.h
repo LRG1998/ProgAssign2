e73109fdd0176798ac3e836ce11e9e8ce656c04a
LRG1998 <ericwarfish212@gmail.com>
2023-08-31 11:20:15 -0500

    added roll function

--- dice.h
+++ dice.h
@@ -1,19 +1,40 @@
+#include <cstdlib>
 #include <iostream>
 #include <random>
 
 class Dice {
 public:
-Dice(int faces{6}, int count {1});
+    Dice(int faces, int count){
+        setFaces(faces);
+        setCount(count);
+    }
 
     void Init(){
         std::cout << "Initialized" << std::endl;
-        getFaces();
         std::cout << faces<<std::endl;
     }
     int getFaces(){
         return faces;
     }
+    int setFaces(int desired){
+        faces = desired;
+        return faces;
+    }
+    int getCount(){
+        return count;
+    }
+    int setCount(int desired){
+        count = desired;
+        return count;
+    }
+    int max(){
+        return faces*count;
+    }
+    int roll(){
+        return rand() % max();
+    }
 
 private:
 int faces{6};
+int count{1};
 };
--- diceRoll.cpp
+++ diceRoll.cpp
@@ -1,7 +1,9 @@
 #include "dice.h"
+#include <iostream>
 
 int main(){
-    Dice dice1;
+    Dice dice1{6,2};
     dice1.Init();
+    std::cout<<dice1.roll()<<"\n" << dice1.roll()<< "\n";
     return 0;
 }
